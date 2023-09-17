79225071971a671fddf91aec8a68d9878d64df00
Jleibo <143144488+Jleibo@users.noreply.github.com>
2023-09-17 13:05:57 -0600

    Delete Programming Basics v22 PT directory

--- Programming Basics v22 PT/VS Code Assignments/Week_1_Assignemts/Leibovitz_Jack_JS_CreateVariables.js
+++
@@ -1,3 +0,0 @@
-//var for rollercoaster fun
-var Minimum_Height = 42;
-var Minimum_Age = 10;
--- Programming Basics v22 PT/VS Code Assignments/Week_1_Assignemts/code.js
+++
@@ -1,7 +0,0 @@
-// set the numebr of likes
-var likeCount = 0;
-//set the count to zero
-function increaseLikes(){
-    //this is addition
-    likeCount = likeCount + 1;
-}
